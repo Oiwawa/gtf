45f596f4ac187828740a97900e9327ea26e6a254
Hugo Radepont <hugoradepont@gmail.com>
2022-05-19 11:25:22 +0200

    tailwind and main page

--- app.vue
+++ app.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-24 h-full shadow-md bg-white px-1 absolute">
+    <div class="relative">
+      <button type="button" class="flex items-center m-5 h-12">
+        <img src="/assets/img/home.png" alt="See previous flag">
+      </button>
+    </div>
+    <div class="relative">
+      <button type="button" class="flex items-center py-4 px-6 h-20">
+        <img src="/assets/img/params.png" alt="See previous flag">
+      </button>
+    </div>
   </div>
+    <div class="grid place-items-center h-screen bg-black h-screen">
+      <div class="">
+        <span class="dark:text-white">-- / --</span>
+      </div>
+      <div>
+        <span class="dark:text-white">00:00</span>
+      </div>
+      <div class="border-2 divide-gray-50 w-96">
+        <img src="/assets/img/uk-flag.png" class="object-cover" alt="Guess this flag!">
+      </div>
+      <div>
+        <label for="flag-name">
+          <input type="text" id="flag-name" name="flag-name">
+        </label>
+      </div>
+      <div class="flex space-x-40">
+        <button type="button" class="w-12">
+          <img src="/assets/img/arrow-left.png" alt="See previous flag">
+        </button>
+        <button type="button" class="w-12">
+          <img src="/assets/img/arrow-right.png" alt="See next flag">
+        </button>
+      </div>
+    </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
 
+    ssr: false,
+    head: {
+        title: 'GTF',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/png', href: '/im/favicon.png' },
+        ]
+    },
+
+    buildModules: [
+        '@nuxtjs/tailwindcss'
+    ],
 })
